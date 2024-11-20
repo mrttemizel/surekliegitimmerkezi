cf245ce9bd6c0061e38a59c41371fafd05e77a92
zeynep.cakir <zeynep.cakir@antalya.edu.tr>
2024-11-20 15:19:36 +0300

    1) eğitim talep 2) organizasyon talep 3) şikayet yönetim istemi eklenmiştir

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'dashboard/organization-chart/upload'
+        'dashboard/organization-chart/upload',
+        '/dashboard/generate-certificates',
     ];
 }
