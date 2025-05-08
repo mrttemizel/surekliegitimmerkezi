3abebbedea9c8834a37c00a9be722d7eeeae3be3
zeynep.cakir <zeynep.cakir@antalya.edu.tr>
2025-05-08 11:35:33 +0300

    sayfa düzenlemeleri

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -168,7 +168,7 @@
                         @endforeach
                     </div>
                     <div class="text-center">
-                        <a href="{{route('tum_egitimler.index')}}" class="view-courses"> Tüm Eğtimleri Görüntüle
+                        <a href="{{route('tum_egitimler.index')}}" class="view-courses"> Tüm Eğitimleri Görüntüle
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                  stroke-linejoin="round" class="feather feather-arrow-right">
